d1c9294042524f27a35306f69cbe04d3e7445655
Ashutosh Jindal <Ashutosh.Jindal@monitise.com>
2012-08-17 14:33:35 +0100

    First Checkin

---
+++ src/A.java
@@ -0,0 +1,16 @@
+import java.io.InputStream;
+
+import org.junit.Test;
+
+public class A {
+
+	@Test
+	public void test() {
+		InputStream stream = A.class.getResourceAsStream("/SomeTextFile.txt");
+        System.out.println(stream != null);
+        stream = Test.class.getClassLoader()
+            .getResourceAsStream("SomeTextFile.txt");
+        System.out.println(stream != null);
+	}
+
+}
