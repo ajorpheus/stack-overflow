f08de9c951dac745237b5f956758143f10d8c120
Ashutosh Jindal <Ashutosh.Jindal@monitise.com>
2012-08-21 12:44:28 +0100

    Test AudioInputStream

--- src/A.java
+++ src/A.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
 import java.io.InputStream;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import org.junit.Test;
 
@@ -11,6 +16,15 @@ public class A {
         stream = Test.class.getClassLoader()
             .getResourceAsStream("SomeTextFile.txt");
         System.out.println(stream != null);
+	}
+
+	@Test
+	public void testAudioInputStream() throws UnsupportedAudioFileException, IOException {
+		 // Probably some try catch over the next statement to log the error if `result.wav` is not found.
+        InputStream stream = A.class.getResourceAsStream("/result.wav");
+        System.out.println(stream != null);
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(stream);
+        System.out.println(audioInputStream != null);
 	}
 
 }
